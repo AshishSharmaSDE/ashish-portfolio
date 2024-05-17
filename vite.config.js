926c10b87efc7acebea37477d777fb26e3d29c89
Ashish Sharma <ashishsharma.sde@google.com>
2024-05-17 14:04:59 +0530

    resolveing deploy issues

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sassPlugin from "vite-plugin-sass"; // Import default export instead
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sassPlugin()],
+  plugins: [react(), sassPlugin()], // Make sure to use sassPlugin as the default export
 });
