7b45424b01326dc105cb7d4341e828b6f7e5b341
Ashish Sharma <ashishsharma.sde@google.com>
2024-05-17 14:45:07 +0530

    removed scss ref from vide.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sassPlugin from "vite-plugin-sass"; // Import default export instead
-
 export default defineConfig({
-  plugins: [react(), sassPlugin()], // Make sure to use sassPlugin as the default export
+  plugins: [react()],
 });
